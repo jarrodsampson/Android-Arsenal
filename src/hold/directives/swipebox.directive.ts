35e72da60e0a535dc5726b8b6617b3fcc7ee6197
Jarrod Sampson <jarrod@gunghoonline.com>
2017-05-09 18:57:03 -0700

    remove hold/ folder

--- src/hold/directives/swipebox.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, ElementRef, Input } from '@angular/core';
-import * as $ from 'jquery';
-
-@Directive({ selector: '[swipeboxer]' })
-export class SwipeDirective {
-  constructor() {
-    $( '.swipebox' ).swipebox({loopAtEnd: true});
-  }
-}
