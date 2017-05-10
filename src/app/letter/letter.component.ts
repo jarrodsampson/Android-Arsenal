2289cd00661c8fd781f25e9447d015e42874f68f
Jarrod Sampson <jarrod@gunghoonline.com>
2017-05-09 19:14:34 -0700

    redo

--- src/app/letter/letter.component.ts
+++ src/app/letter/letter.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import { APIService } from '../services/APIService';
+import {RepoStore} from "../services/Store";
+import { MzToastService } from 'ng2-materialize';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +13,15 @@ export class LetterComponent implements OnInit {
 
   letter = "";
   searchList = [];
+  savedFavorites = [];
+  repoStore: RepoStore;
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router, private _apiService: APIService) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private _apiService: APIService, repoStore: RepoStore, private toastService: MzToastService) {
+    this.repoStore = repoStore;
+    for (var i = 0; i < this.repoStore.repos.length;++i) {
+      this.savedFavorites.push(this.repoStore.repos[i].title);
+    }
+  }
 
   ngOnInit() {
 
@@ -36,6 +45,18 @@ export class LetterComponent implements OnInit {
   detailsView(repo) {
     console.log(repo.id);
     this.router.navigate(['./details/' + repo.id]);
+  }
+
+  saveRepo(repo) {
+    // check if already saved
+    if (this.savedFavorites.indexOf(repo.title) > -1) {
+      this.toastService.show(repo.title + " is already saved!", 2500);
+    } else {
+      //console.log("Added: " + repo.title);
+      this.repoStore.add(repo.title, repo.id, repo.link, repo.description, repo.tagName);
+      this.toastService.show("Added: " + repo.title, 2500);
+      this.savedFavorites.push(repo.title);
+    }
   }
 
 }
